7496f0a2a7e636b9ab75ce013f58588b2f72e002
so1yana <trapbigest@gmai.com>
2023-12-22 17:45:27 +0300

    added the necessary imports

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log('hi');
+import { createRoot } from 'react-dom/client';
+import { Component } from 'react';
+import FilmList from './components/film-list';
+import Pagination from './components/pagination';
+import SearchPanel from './components/search-panel';
+import Tabs from './components/tabs';
+
+import './index.css';
+import './reset.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="page">
+                <Tabs />
+                <SearchPanel />
+                <FilmList />
+                <Pagination />
+            </div>
+        );
+    }
+}
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(<App />);
