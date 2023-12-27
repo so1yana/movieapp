92fad4b982586cad0487ec591df90a44a059a981
so1yana <trapbigest@gmai.com>
2023-12-27 15:50:37 +0300

    amend

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { Component } from 'react';
 import FilmList from './components/film-list';
 import Pages from './components/pagination';
 import TabsApp from './components/tabs';
+import Spinner from './components/spinner';
+import Info from './components/info';
 
 import './index.css';
 import './reset.css';
@@ -11,9 +13,13 @@ class App extends Component {
     state = {
         movies: [],
         pages: 1,
+        searching: false,
+        status: 'Ready',
     };
 
     searchMovie = (query, page = 1) => {
+        this.setState({ searching: true, movies: [] });
+
         const options = {
             method: 'GET',
             headers: {
@@ -30,18 +36,35 @@ class App extends Component {
         )
             .then((response) => response.json())
             .then((response) => {
-                this.setState({ movies: response.results, pages: response.total_results });
+                this.setState({
+                    movies: response.results,
+                    pages: response.total_results,
+                    searching: false,
+                    status: 'ok',
+                });
             })
-            .catch((err) => () => err);
+            .catch((err) => this.setState({ searching: false, status: err }));
     };
 
+    check() {
+        const { movies, searching, status } = this.state;
+
+        if (movies.length > 0) return <FilmList movies={movies} />;
+        if (searching) return <Spinner />;
+        if (status !== 'ok') {
+            if (typeof status === 'object') return <Info text="Something went wrong..." />;
+        } else if (status === 'ok' && movies.length === 0)
+            return <Info text="Nothing was found..." />;
+        return null;
+    }
+
     render() {
-        const { movies, pages } = this.state;
+        const { pages } = this.state;
 
         return (
             <div className="page">
                 <TabsApp searchMovie={this.searchMovie} />
-                <FilmList movies={movies} />
+                {this.check()}
                 <Pages pagesCount={pages} />
             </div>
         );
