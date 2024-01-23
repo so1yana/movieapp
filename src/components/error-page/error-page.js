e378ec9043ffac7123b6eaaab814321a0e714dab
so1yana <trapbigest@gmai.com>
2024-01-23 21:46:03 +0300

    initial

---
+++ src/components/error-page/error-page.js
@@ -0,0 +1,12 @@
+import './error-page.css';
+
+export default function ErrorPage() {
+    return (
+        <div className="error-page">
+            <h1>
+                Something went wrong
+                <p>Try turning on VPN or something else</p>
+            </h1>
+        </div>
+    );
+}
